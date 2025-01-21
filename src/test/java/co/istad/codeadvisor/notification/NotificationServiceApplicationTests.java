626b034eee6d5a6c0b0642fc47f8a33fa1d183b3
vanndeth <artvandeth2001@gmail.com>
2025-01-21 11:26:01 +0700

    comment test folder

--- src/test/java/co/istad/codeadvisor/notification/NotificationServiceApplicationTests.java
+++ src/test/java/co/istad/codeadvisor/notification/NotificationServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package co.istad.codeadvisor.notification;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NotificationServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package co.istad.codeadvisor.notification;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NotificationServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
